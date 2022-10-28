94e6b95e50171a240a47938062a0fb18c7a9ba0d
prasanjitdeka <prasanjitdeka@gmail.com>
2022-10-27 23:55:51 -0500

    commit for login

--- storeflexservice/src/main/java/com/storeflex/config/AppConfiguration.java
+++ storeflexservice/src/main/java/com/storeflex/config/AppConfiguration.java
@@ -16,9 +16,21 @@ import lombok.ToString;
 @Component
 public class AppConfiguration {
 	
-	@Value("${testuser.username}")
-	private String testuser;
-	@Value("${testuser.password}")
-	private String password;
+	@Value("${testuser.username_temp}")
+	private String test_user;
+	@Value("${testuser.password_temp}")
+	private String test_password;
+	@Value("${testuser.username_SL}")
+	private String sl_user;
+	@Value("${testuser.password_SL}")
+	private String sl_password;
+	@Value("${testuser.username_CL}")
+	private String cl_user;
+	@Value("${testuser.password_CL}")
+	private String cl_password;
+	@Value("${testuser.username_CU}")
+	private String cust_user;
+	@Value("${testuser.password_CU}")
+	private String cust_password;
 
 }
