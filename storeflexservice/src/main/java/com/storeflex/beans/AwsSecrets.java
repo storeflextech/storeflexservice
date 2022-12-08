1cfbd642b29a7c842a1f2bd483eecd4f55710c38
D0D3 <pdeka@skysolutions.com>
2022-12-08 10:42:47 -0600

    Commit for AWS sercet manager implementation for DB credentails

---
+++ storeflexservice/src/main/java/com/storeflex/beans/AwsSecrets.java
@@ -0,0 +1,21 @@
+package com.storeflex.beans;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class AwsSecrets {
+
+    private String username;
+    private String password;
+    private String host;
+    private String engine;
+    private String port;
+    private String dbInstanceIdentifier;
+}
--- storeflexservice/src/main/java/com/storeflex/config/AppConfiguration.java
+++ storeflexservice/src/main/java/com/storeflex/config/AppConfiguration.java
@@ -2,9 +2,22 @@ package com.storeflex.config;
 
 import java.io.Serializable;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.google.gson.Gson;
+import com.storeflex.beans.AwsSecrets;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -65,4 +78,65 @@ public class AppConfiguration implements Serializable{
 	private String zohoClientSecret;
 	@Value("${signing.zoho.refreshToken}")
 	private String zohoRefreshToken;
+	
+	@Value("${cloud.aws.credentails.access-key}")
+	private String accessKey;
+	@Value("${cloud.aws.credentails.secret-key}")
+	private String secretKey;
+	
+	@Value("${storeflexaws.dbsecret:name}")
+	private String dbsecret;
+	@Value("${storeflexaws.dbsecret:region}")
+	private String region;
+	
+	@Value("${postgres.driver}")
+	private String driver;
+	@Value("${postgres.db}")
+	private String jdbcName;
+	@Value("${postgres.dbname}")
+	private String dbName;
+	
+
+	//create a parsing object 
+	private Gson gson = new Gson();
+	
+	//primary DB loading
+	@Bean
+	public DataSource dataSource() {
+		AwsSecrets secrets = getSecret();
+		return DataSourceBuilder.create()
+				.driverClassName(driver).url(jdbcName+":" + secrets.getEngine() + "://"
+						+ secrets.getHost() + ":" + secrets.getPort() + "/"+dbName)
+				.username(secrets.getUsername()).password(secrets.getPassword()).build();
+	}
+	
+	//DB scrcet manager
+	private AwsSecrets getSecret() {
+
+		String secretName = "dev/storeflexDB";
+		String awsRegion = "ap-south-1";
+		
+		AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard()
+				.withRegion(awsRegion)
+				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey,secretKey)))
+				.build();
+
+		String secret =null;
+
+		GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName);
+		GetSecretValueResult getSecretValueResult = null;
+
+		try {
+			getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+		} catch (Exception e) {
+			throw e;
+		}
+		if (getSecretValueResult.getSecretString() != null) {
+			secret = getSecretValueResult.getSecretString();
+			return gson.fromJson(secret, AwsSecrets.class);
+		}
+		return null;
+
+	}
+	
 }
